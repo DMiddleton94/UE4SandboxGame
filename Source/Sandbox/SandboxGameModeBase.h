6f288b203fad00547768fe2918091cfeec0f89c0
Darren Middleton <k1462486@kcl.ac.uk>
2017-07-31 23:49:04 +0100

    Initial Game

---
+++ Source/Sandbox/SandboxGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SandboxGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SANDBOX_API ASandboxGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
